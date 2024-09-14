8df1faa93fca52e92bcf48db9b0c658c1114c0e2
Matthew <mattjmears371@gmail.com>
2024-09-14 17:09:09 +0000

    Welcome message to player, display of board

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randrange
+
+ship_initial = ["B", "C", "F", "A", "S"]
+ship_names = ["Battleship", "Cruiser", "Frigate", "Aircraft Carrier", "Sub"]
+map_size = 10
+
+
+def get_username():
+    """
+    function getting username for welcome message
+    """
+    while True:
+        user_name = input("Enter your name: ")
+        if user_name:
+            print(f"Welcome to the battleship game {user_name}!")
+            return user_name
+        else:
+            print("Please enter your name.")
+
+
+def create_battlefield(map_size):
+    """
+    function to create a map based on size
+    """
+    return [["_"] * map_size for _ in range(map_size)]
+
+
+def display_battlefield(board):
+    """
+    function to display current state of the map
+    """
+    for row in board:
+        print(" ".join(row))
